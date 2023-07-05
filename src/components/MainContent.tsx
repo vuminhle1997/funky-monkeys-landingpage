c1969bc579a6b9e465676f175b858e96e6a32ac9
Vu Minh Le <vuminhle@outlook.com>
2023-07-05 22:20:27 +0200

    BS

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ShowImg from '../img/show.jpg';
 import WipImg from '../img/placeholder.jpg';
+import XMAS_SHOW from '../img/tfm-xmas.jpg';
+import MINA_FROM_TWICE from '../img/mina.jpg';
+import classNames from 'classnames';
 
 const socials = [
   {
@@ -28,15 +31,53 @@ export default function MainContent({
   handleDisclaimer: () => void;
   handleContact: () => void;
 }) {
+  /**
+   *
+   * @returns boolean
+   */
+  const checksToansStupidXMASRequest = (): boolean => {
+    const currentStupidBullshitDate = new Date();
+
+    const stupidStartingXMASDate = new Date(
+      `June 1, ${currentStupidBullshitDate.getFullYear()}`
+    );
+    const stupidEndXMASDate = new Date(
+      `December 24, ${currentStupidBullshitDate.getFullYear()}`
+    );
+
+    const stupidStartingTimeInMS = stupidStartingXMASDate.getTime();
+    const stupidEndXMASTimeInMS = stupidEndXMASDate.getTime();
+    const currentTimeInMS = currentStupidBullshitDate.getTime();
+
+    return (
+      currentTimeInMS > stupidStartingTimeInMS &&
+      currentTimeInMS < stupidEndXMASTimeInMS
+    );
+  };
+
+  const stupidClassName = classNames({
+    'col-md-4 my-3 my-md-0 we-like-mina-sana-momo':
+      checksToansStupidXMASRequest(),
+    'col-md-6 my-3 my-md-0 we-dont-like-jennie-from-blackpink':
+      !checksToansStupidXMASRequest(),
+  });
+
   return (
     <div className={'container expand'} id="linda-content">
+      <div className="d-none">
+        <img src={MINA_FROM_TWICE} alt="MINA_FROM_TWICE" />
+        <img src={MINA_FROM_TWICE} alt="MINA_FROM_TWICE" />
+        <img src={MINA_FROM_TWICE} alt="MINA_FROM_TWICE" />
+        <img src={MINA_FROM_TWICE} alt="MINA_FROM_TWICE" />
+        <img src={MINA_FROM_TWICE} alt="MINA_FROM_TWICE" />
+      </div>
       <div className="justify-content-center align-items-center d-flex flex-column vh-100">
         <div className="twice-mina-momo-sana p-5 d-flex justify-content-center flex-column align-items-center">
           <h1 id="title" className="mb-3">
             THE FUNKY MONKEYS
           </h1>
           <div className="row">
-            <div className="col-md-6 my-3 my-md-0">
+            <div className={stupidClassName}>
               <a
                 href="https://funky-monkeys.show"
                 target="_blank"
@@ -58,7 +99,32 @@ export default function MainContent({
                 </div>
               </a>
             </div>
-            <div className="col-md-6 my-md-0" id="fm-org">
+
+            {checksToansStupidXMASRequest() && (
+              <div className={stupidClassName}>
+                <a
+                  href="https://funky-monkeys.show/specials/xmas-special"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="w-100 hover-this" id="fm-show">
+                    <div className="position-relative">
+                      <img
+                        src={XMAS_SHOW}
+                        width={300}
+                        height={300}
+                        alt="The link to the Show site."
+                      />
+                      <div className="position-absolute bottom-0 w-100 text-bg-dark p-3 text-center special">
+                        <strong>XMAS DELIGHT</strong>
+                      </div>
+                      <div className="position-absolute top-0 w-100 h-100 text-bg-dark d-flex justify-content-center align-items-center opacity-25"></div>
+                    </div>
+                  </div>
+                </a>
+              </div>
+            )}
+            <div className={stupidClassName} id="fm-org">
               {/* TODO: add Org website when it is ready, with "rel" and "target" */}
               <a href="#">
                 <div className="w-100 hover-this disabled">
